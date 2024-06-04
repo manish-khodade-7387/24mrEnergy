37d1a6fca79a4b75d42f32b49f81d75e95cac457
manish-khodade-7387 <manishkhodade9088@gmail.com>
2024-06-04 16:01:28 +0530

    Update AboutUsController.php

--- AboutUsController.php
+++ AboutUsController.php
@@ -6,7 +6,7 @@ use CodeIgniter\Controller;
 
 class AboutUsController extends Controller
 {
-    public function About()
+    public function About_Us()
     {
         return view('About_Us');
     }
